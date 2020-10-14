4223f38a17a7e08fcf990025ccafd92beacac07a
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-10-14 10:53:39 +0100

    Update aws.tf

--- modules/aws.tf
+++ modules/aws.tf
@@ -166,6 +166,24 @@ ingress {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
  
   ingress {
     from_port   = 30000
