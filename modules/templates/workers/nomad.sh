a9b97060a6dc3814e7a539bdf8cdfa89a07ff48d
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-10-21 15:55:21 +0100

    Update nomad.sh

--- modules/templates/workers/nomad.sh
+++ modules/templates/workers/nomad.sh
@@ -165,11 +165,8 @@ echo "--> Running  Nomad Job"
 
  nomad run hashibo.nomad
  nomad run nginx-pki.nomad
- nomad run hackaton_q3/payments.nomad
- nomad run hackaton_q3/product-db.nomad
- nomad run hackaton_q3/product-api.nomad
- nomad run hackaton_q3/public-api.nomad
- nomad run hackaton_q3/frontend.nomad
+ nomad run hackaton_q3/hashicups.nomad
+
 
 fi
 
