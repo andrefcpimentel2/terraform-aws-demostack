01a37359d75059a6549f75fcec96a824dd4f440c
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2021-05-26 10:17:12 +0100

    Update boundary-lb.tf

--- modules/boundary-lb.tf
+++ modules/boundary-lb.tf
@@ -1,52 +1,52 @@
 
-resource "aws_lb" "boundary-controller" {
-   name = "${var.namespace}-boundary-cont"
-  load_balancer_type = "network"
-  internal           = false
-subnets         = aws_subnet.demostack.*.id
+# resource "aws_lb" "boundary-controller" {
+#    name = "${var.namespace}-boundary-cont"
+#   load_balancer_type = "network"
+#   internal           = false
+# subnets         = aws_subnet.demostack.*.id
 
- tags = {
-    Name           = "${var.namespace}-boundary-cont"
-    owner          = var.owner
-    created-by     = var.created-by
-    sleep-at-night = var.sleep-at-night
-    TTL            = var.TTL
-  }
-}
+#  tags = {
+#     Name           = "${var.namespace}-boundary-cont"
+#     owner          = var.owner
+#     created-by     = var.created-by
+#     sleep-at-night = var.sleep-at-night
+#     TTL            = var.TTL
+#   }
+# }
 
-resource "aws_lb_target_group" "boundary-controller" {
-  name     = "${var.namespace}-boundary-cont"
-  port     = 9200
-  protocol = "TCP"
-  vpc_id   = aws_vpc.demostack.id
+# resource "aws_lb_target_group" "boundary-controller" {
+#   name     = "${var.namespace}-boundary-cont"
+#   port     = 9200
+#   protocol = "TCP"
+#   vpc_id   = aws_vpc.demostack.id
 
-  stickiness  {
-    enabled = false
-     type    = "lb_cookie"
-  }
-  tags = {
-    Name           = "${var.namespace}-boundary-cont"
-    owner          = var.owner
-    created-by     = var.created-by
-    sleep-at-night = var.sleep-at-night
-    TTL            = var.TTL
-  }
-}
+#   stickiness  {
+#     enabled = false
+#      type    = "lb_cookie"
+#   }
+#   tags = {
+#     Name           = "${var.namespace}-boundary-cont"
+#     owner          = var.owner
+#     created-by     = var.created-by
+#     sleep-at-night = var.sleep-at-night
+#     TTL            = var.TTL
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "boundary-controller" {
-  count            = var.servers
-  target_group_arn = aws_lb_target_group.boundary-controller.arn
-  target_id        = aws_instance.servers[count.index].id
-  port             = 9200
-}
+# resource "aws_lb_target_group_attachment" "boundary-controller" {
+#   count            = var.servers
+#   target_group_arn = aws_lb_target_group.boundary-controller.arn
+#   target_id        = aws_instance.servers[count.index].id
+#   port             = 9200
+# }
 
-resource "aws_lb_listener" "boundary-controller" {
-  load_balancer_arn = aws_lb.boundary-controller.arn
-  port              = "9200"
-  protocol          = "TCP"
+# resource "aws_lb_listener" "boundary-controller" {
+#   load_balancer_arn = aws_lb.boundary-controller.arn
+#   port              = "9200"
+#   protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.boundary-controller.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.boundary-controller.arn
+#   }
+# }
