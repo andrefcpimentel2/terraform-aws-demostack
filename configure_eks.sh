a7367bb2b7665cbc0d6dbce801829959ae506e98
andrefcpimentel2 <andre@hashicorp.comp>
2025-05-15 18:13:48 +0200

    initial boostrap

--- configure_eks.sh
+++ configure_eks.sh
@@ -1,4 +1,4 @@
-aws eks --region eu-west-2 update-kubeconfig --name guystack1-eks
+aws eks --region eu-west-2 update-kubeconfig --name andrestack1-eks
 # arn:aws:eks:eu-west-2:958215610051:cluster/guystack1-eks
 
 kubectl config get-contexts
