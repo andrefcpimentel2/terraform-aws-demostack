d7093c5db534d05995b18bf2f09e65071a85870d
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2025-07-30 09:15:10 +0200

    Update configure_eks.sh

--- configure_eks.sh
+++ configure_eks.sh
@@ -1,7 +1,6 @@
 aws eks --region eu-west-2 update-kubeconfig --name andrestack1-eks
 # arn:aws:eks:eu-west-2:958215610051:cluster/guystack1-eks
 
-aws_key = "ASIAY34FZKBOKMUTVV7A"
 github_token = "e442cf825b0294f35ff01390c19f42759e7f15024"
 
 kubectl config get-contexts
