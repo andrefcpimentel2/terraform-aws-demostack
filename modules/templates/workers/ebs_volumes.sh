80ce164497d8cf5068ea18f1c815a4afc434f93f
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2026-02-10 16:07:13 +0100

    removing references (commenting on provisioners not enought?)

--- modules/templates/workers/ebs_volumes.sh
+++ modules/templates/workers/ebs_volumes.sh
@@ -1,116 +1,8 @@
 #!/usr/bin/env bash
 
-# echo "==> getting the aws metadata token"
-# export TOKEN=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
-# echo "==> check token was set"
-# echo $TOKEN
 
 echo "--> Configuring EBS mounts"
-
-# export NOMAD_ADDR=https://$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4):4646
-
-# echo "--> Create EBS CSI plugin job"
-# {
-# sudo tee  /etc/nomad.d/default_jobs/plugin-ebs-controller.nomad > /dev/null <<EOF
-# job "plugin-aws-ebs-controller" {
-#   datacenters = ["${dc1}","${dc2}","${dc3}"]
-
-#   group "controller" {
-#     task "plugin" {
-#       driver = "docker"
-
-#       config {
-#         image = "amazon/aws-ebs-csi-driver:latest"
-
-#         args = [
-#           "controller",
-#           "--endpoint=unix://csi/csi.sock",
-#           "--logtostderr",
-#           "--v=5",
-#         ]
-#       }
-
-#       csi_plugin {
-#         id        = "aws-ebs0"
-#         type      = "controller"
-#         mount_dir = "/csi"
-#       }
-
-#       resources {
-#         cpu    = 500
-#         memory = 256
-#       }
-#     }
-#   }
-# }
-# EOF
-# } || {
-#     echo "--> CSI plugin job skipped"
-# }
-# echo "--> Create Nodes CSI plugin job"
-# {
-# sudo tee  /etc/nomad.d/default_jobs/plugin-ebs-nodes.nomad > /dev/null <<EOF
-# job "plugin-aws-ebs-nodes" {
-#   datacenters = ["${dc1}","${dc2}","${dc3}"]
-
-#   # you can run node plugins as service jobs as well, but this ensures
-#   # that all nodes in the DC have a copy.
-#   type = "system"
-
-#   group "nodes" {
-#     task "plugin" {
-#       driver = "docker"
-
-#       config {
-#         image = "amazon/aws-ebs-csi-driver:latest"
-
-#         args = [
-#           "node",
-#           "--endpoint=unix://csi/csi.sock",
-#           "--logtostderr",
-#           "--v=5",
-#         ]
-
-#         # node plugins must run as privileged jobs because they
-#         # mount disks to the host
-#         privileged = true
-#       }
-
-#       csi_plugin {
-#         id        = "aws-ebs0"
-#         type      = "node"
-#         mount_dir = "/csi"
-#       }
-
-#       resources {
-#         cpu    = 500
-#         memory = 256
-#       }
-#     }
-#   }
-# }
-# EOF
-# } || {
-#     echo "--> Nodes job skipped"
-# }
-# echo "--> Mysql"
-# {
-# sudo tee  /etc/nomad.d/default_jobs/mysql_ebs_volume.hcl > /dev/null <<EOF
-# # volume registration
-# type = "csi"
-# id = "mysql"
-# name = "mysql"
-# volume_id = "${aws_ebs_volume_mysql_id}"
-# capability {
-#   access_mode     = "single-node-writer"
-#   attachment_mode = "file-system"
-# }
-# plugin_id = "aws-ebs0"
-# EOF
-# } || {
-#     echo "--> Mysql failed, probably already done"
-# }
 
 echo "--> Ollama"
 {
@@ -129,90 +21,17 @@ EOF
     echo "--> ollama failed, probably already done"
 }
 
-# echo "--> Mongodb"
-# {
-# sudo tee  /etc/nomad.d/default_jobs/mongodb_ebs_volume.hcl > /dev/null <<EOF
-# # volume registration
-# type = "csi"
-# id = "mongodb"
-# name = "mongodb"
-# volume_id = "${aws_ebs_volume_mongodb_id}"
-# capability {
-#   access_mode     = "single-node-writer"
-#   attachment_mode = "file-system"
-# }
-# plugin_id = "aws-ebs0"
-# EOF
-
-
-# } || {
-#     echo "--> MongoDB failed, probably already done"
-# }
-
-# echo "--> Prometheus"
-# {
-# sudo tee  /etc/nomad.d/default_jobs/prometheus_ebs_volume.hcl > /dev/null <<EOF
-# # volume registration
-# type = "csi"
-# id = "prometheus"
-# name = "prometheus"
-# volume_id = "${aws_ebs_volume_prometheus_id}"
-# capability {
-#   access_mode     = "single-node-writer"
-#   attachment_mode = "file-system"
-# }
-# plugin_id = "aws-ebs0"
-# EOF
-# } || {
-#     echo "--> Prometheus failed, probably already done"
-# }
-# echo "--> Shared"
-# {
-# sudo tee  /etc/nomad.d/default_jobs/shared_ebs_volume.hcl > /dev/null <<EOF
-# # volume registration
-# type = "csi"
-# id = "shared"
-# name = "shared"
-# volume_id = "${aws_ebs_volume_shared_id}"
-# capability {
-#   access_mode     = "single-node-writer"
-#   attachment_mode = "file-system"
-# }
-# plugin_id = "aws-ebs0"
-# EOF
-# } || {
-#     echo "--> Shared failed, probably already done"
-# }
-
-# if [ ${index} == ${count} ]
-# then
-# echo "--> last worker, lets do this"
 ####
-echo "--> Waiting for Nomad leader"
-while ! curl -s -k https://$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4):4646/v1/status/leader --show-error; do
-  sleep 2
-done
 
-# echo "--> Waiting for a list of Nomad peers"
-# while ! curl -s -k https://$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4):4646/v1/status/peers --show-error; do
-#   sleep 2
-# done
 
 echo "--> Waiting for all Nomad servers"
 while [ "$(nomad server members 2>&1 | grep "alive" | wc -l)" -lt "${nomad_servers}" ]; do
   sleep 5
 done
-####
-# nomad run  /etc/nomad.d/default_jobs/plugin-ebs-controller.nomad
-# sleep 30
-# nomad run  /etc/nomad.d/default_jobs/plugin-ebs-nodes.nomad
 
-# sleep 30
-# nomad volume register /etc/nomad.d/default_jobs/mongodb_ebs_volume.hcl
+
 nomad volume register /etc/nomad.d/default_jobs/ollama_ebs_volume.hcl
-else
-echo "--> not the last worker, skip"
-fi
+
 
 
 echo "==> Configuring EBS mounts is Done!"
