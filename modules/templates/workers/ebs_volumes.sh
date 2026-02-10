c4150c9306cb081e26f02fd8b38fccbc4d13d6cb
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2026-02-10 16:39:03 +0100

    getting node_id dynamically

--- modules/templates/workers/ebs_volumes.sh
+++ modules/templates/workers/ebs_volumes.sh
@@ -2,13 +2,25 @@
 
 
 
+
+####
+
+
+echo "--> Waiting for all Nomad servers"
+while [ "$(nomad server members 2>&1 | grep "alive" | wc -l)" -lt "${nomad_servers}" ]; do
+  sleep 5
+done
+
 echo "--> Configuring EBS mounts"
+
+export NODE_ID=$(nomad node status -self | grep ID | awk -F '=' '{print $2}'
 
 echo "--> Ollama"
 {
 sudo tee  /etc/nomad.d/default_jobs/ollama_ebs_volume.hcl > /dev/null <<EOF
 # volume registration
 type  = "host"
+node_id = "${NODE_ID}"
 name = "ollama"
 capacity = "80GB"
 capability {
@@ -21,13 +33,6 @@ EOF
     echo "--> ollama failed, probably already done"
 }
 
-####
-
-
-echo "--> Waiting for all Nomad servers"
-while [ "$(nomad server members 2>&1 | grep "alive" | wc -l)" -lt "${nomad_servers}" ]; do
-  sleep 5
-done
 
 
 nomad volume register /etc/nomad.d/default_jobs/ollama_ebs_volume.hcl
