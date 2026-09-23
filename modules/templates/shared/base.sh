d8604369d209c3eda51677d418602dc5ca3fbd53
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2026-09-23 08:47:09 +0200

    commenting DD monitoring for now

--- modules/templates/shared/base.sh
+++ modules/templates/shared/base.sh
@@ -182,14 +182,17 @@ fi
 
 #Setup Datadog Monitoring Agent
 
-DD_API_KEY=${dd_api_key} \
-DD_SITE="datadoghq.eu" \
-DD_APM_INSTRUMENTATION_ENABLED=host \
-DD_DATA_STREAMS_ENABLED=true \
-DD_PROFILING_ENABLED=auto \
-DD_REMOTE_CONFIGURATION_ENABLED=true \
-DD_APM_INSTRUMENTATION_LIBRARIES=java:1,python:4,js:5,php:1,dotnet:3,ruby:2 \
-DD_LOGS_CONFIG_PROCESS_COLLECT_ALL=true \
-bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+# DD_API_KEY=${dd_api_key} \
+# DD_SITE="datadoghq.eu" \
+# DD_APM_INSTRUMENTATION_ENABLED=host \
+# DD_DATA_STREAMS_ENABLED=true \
+# DD_PROFILING_ENABLED=auto \
+# DD_REMOTE_CONFIGURATION_ENABLED=true \
+# DD_APM_INSTRUMENTATION_LIBRARIES=java:1,python:4,js:5,php:1,dotnet:3,ruby:2 \
+# DD_LOGS_CONFIG_PROCESS_COLLECT_ALL=true \
+# bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+
+
+# sudo tee /etc/datadog-agent/datadog.yaml > /dev/null <<"EOF"
 
 echo "==> Base is done!"
